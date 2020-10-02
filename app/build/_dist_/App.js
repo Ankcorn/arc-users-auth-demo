91d37eeae13cb971487ea77e2953ef07e38c582b
yeo-yeo <gillian@near.st>
2020-10-02 23:27:00 +0100

    adding app

---
+++ app/build/_dist_/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from '../web_modules/react.js';
+import logo from './logo.svg.proxy.js';
+
+function App() {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "bg-blue-600"
+  }, /*#__PURE__*/React.createElement("header", {
+    className: ""
+  }, "help"));
+}
+
+export default App;
